22ebfe5bf17ab0d70d260eff3b39330373851a6e
Muhammad Sufyan MALIK <muhammad.sufyan@ardgrup.com.tr>
2023-07-25 10:58:13 +0300

    Add project files.

---
+++ CoreModule.cs
@@ -0,0 +1,25 @@
+﻿using CrossCuttingConcerns;
+using IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+
+namespace DependencyResolver
+{
+    public class CoreModule : ICoreModule
+    {
+        private CacheTypes _cacheType;
+        public void Load(IServiceCollection serviceCollection)
+        {
+            //does injection for IMemoryCache
+            serviceCollection.AddMemoryCache();
+            serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
+            serviceCollection.AddSingleton<Stopwatch>();
+            if (_cacheType == CacheTypes.MicrosoftMemoryCache)
+            {
+                serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
+            }
+        }
+    }
+}
